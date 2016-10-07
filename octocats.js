9d8c25eeb237d293c3339f36e7ae8a0396a6597b
LucasPas <7lpasiewicz@stpatrick.org>
2016-10-07 10:19:30 -0500

    Made the slap function work, fixed typing errors

--- octocats.js
+++ octocats.js
@@ -1,32 +1,19 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
-function.Octocat(name,arm){
-
+function Octocat(name,arm){
   this.Name = name; // Inputs
   this.Arm = arm;
 
-  var Luke = new.Octocat(Luke,2); // New OctoCats
-  var Paul = new.Octocat(Paul,5);
-  var Mike = new.Octocat(Mike,4);
-
-  for(var b=0; b<arm; b++){
-    console.log ("SLAP");
+  this.Slap = function(){
+    for(var b=0; b<this.Arm; b++){
+      console.log ("SLAP");
+      }
     }
-
 }
-Octocat("Bob",10)
-
-
-
-}
-
-
-
-
-
-
-
-
-
 
 //Write your 3 new octocat objects below here.
+var Luke = new Octocat("Luke",2);
+var Paul = new Octocat("Paul",5);
+var Mike = new Octocat("Mike",4);
+
+Mike.Slap();
